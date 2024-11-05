f6ab5fa02d7e72a77990b091201d9f544650a255
Quentin ROUVILLE <rouvilleq@gmail.com>
2024-11-05 14:38:11 +0100

    First commit

---
+++ src/main/java/isea/poo/App.java
@@ -0,0 +1,13 @@
+package isea.poo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
